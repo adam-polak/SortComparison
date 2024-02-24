82155083301b8919ca5397634cc2061f1c963b83
adampolak <adampolak@10.22.194.200>
2024-02-24 12:32:10 -0600

    adding buttons

--- src/sort/MyFrame.java
+++ src/sort/MyFrame.java
@@ -1,11 +1,16 @@
 package sort;
 
+import java.awt.event.*;
+
 import javax.swing.*;
 
 public class MyFrame extends JFrame {
 
 	private static final long serialVersionUID = -8849683656483583890L;
 	
+	private JButton randomize;
+	private JButton sort;
+	private Number instance;
 	
 	public MyFrame() {
 		initComponents();
@@ -14,6 +19,31 @@ public class MyFrame extends JFrame {
 	private void initComponents() {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle("Sorting Algorithms");
+		
+		instance = new Number();
+		
+		randomize = new JButton();
+			randomize.setText("Randomize");
+				randomize.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						randomizeButtonActionPerformed(e);
+					}
+				});
+		sort = new JButton();
+			sort.setText("Sort Numbers");
+				sort.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						sortButtonActionPerformed(e);
+					}
+				});
+	}
+	
+	private void randomizeButtonActionPerformed(ActionEvent e) {
+		instance.randomGen();
+	}
+	
+	private void sortButtonActionPerformed(ActionEvent e) {
+		instance.insertionSort();
 	}
 	
 	
